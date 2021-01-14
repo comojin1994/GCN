f3ad0da03652215d30b5d2e346c7dddbdd8981cc
comojin1994 <comojin1994@gmail.com>
2021-01-14 21:25:39 +0900

    Add inception model

--- layers.py
+++ layers.py
@@ -1,25 +1,31 @@
 import tensorflow as tf
+import config as C
 
 class GraphConv(tf.keras.layers.Layer):
-    def __init__(self, filters, activation=None, bn=False, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros'):
+    def __init__(self, filters, activation=None, bn=False, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None):
         super(GraphConv, self).__init__()
         self.filters = filters
         self.activation = activation
         self.use_bn = bn
         self.use_bias = use_bias
         self.kernel_initializer = kernel_initializer
+        self.kernel_regularizer = kernel_regularizer
         self.bias_initializer = bias_initializer
+        self.bias_regularizer = bias_regularizer
         self.BatchNorm = tf.keras.layers.BatchNormalization()
         
     def build(self, input_shape):
+        print(f'LOG >>> input shape: {input_shape}')
         self.weight = self.add_weight(name='weight',
                                        shape=[input_shape[1][-1], self.filters],
                                        initializer=self.kernel_initializer,
+                                       regularizer=self.kernel_regularizer,
                                        trainable=True)
         if self.use_bias:
             self.bias = self.add_weight(name='bias',
                                        shape=self.filters,
                                        initializer=self.bias_initializer,
+                                       regularizer=self.bias_regularizer,
                                        trainable=True)
         
     def call(self, inputs):
@@ -59,3 +65,47 @@ class Readout(tf.keras.layers.Layer):
         if self.activation != None:
             output = self.activation(output)
         return output
+
+class InceptionBlock(tf.keras.layers.Layer):
+    def __init__(self, filters):
+        super(InceptionBlock, self).__init__()
+
+        self.gcn1 = GraphConv(filters,
+                                    use_bias=False,
+                                    kernel_regularizer=tf.keras.regularizers.l2(C.l2_reg))
+        
+        self.gcn2_1 = GraphConv(filters,
+                                    use_bias=False,
+                                    kernel_regularizer=tf.keras.regularizers.l2(C.l2_reg))
+        self.gcn2_2 = GraphConv(filters,
+                                    use_bias=False,
+                                    kernel_regularizer=tf.keras.regularizers.l2(C.l2_reg))
+
+        self.gcn3_1 = GraphConv(filters,
+                                    use_bias=False,
+                                    kernel_regularizer=tf.keras.regularizers.l2(C.l2_reg))
+        self.gcn3_2 = GraphConv(filters,
+                                    use_bias=False,
+                                    kernel_regularizer=tf.keras.regularizers.l2(C.l2_reg))
+        self.gcn3_3 = GraphConv(filters,
+                                    use_bias=False,
+                                    kernel_regularizer=tf.keras.regularizers.l2(C.l2_reg))
+
+        self.average = tf.keras.layers.Average()
+
+    def call(self, input_tensor, training=False):
+        A, x = input_tensor
+        A, f1 = self.gcn1([A, x])
+
+        A, f2 = self.gcn2_1([A, x])
+        A, f2 = self.gcn2_2([A, f2])
+
+        A, f3 = self.gcn3_1([A, x])
+        A, f3 = self.gcn3_2([A, f3])
+        A, f3 = self.gcn3_3([A, f3])
+        
+        output = self.average([f1, f2, f3])        
+        
+        return output
+        
+        
